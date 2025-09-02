9464d5127b0e2652fa062e8d11ce41af526c82f1
Joao Albuquerque <joao.albuquerque.t0131873@edu.atec.pt>
2025-09-02 19:11:22 +0100

    adicionei comentarios

--- Exercicios/exercicio1.py
+++ Exercicios/exercicio1.py
@@ -5,23 +5,23 @@ nome=input("Escreve um nome: ")
 
 for char in nome:
 
-    if not (ord(char)==32 or 65<=ord(char)<=90 or 97<=ord(char)<=122):
+    if not (ord(char)==32 or 65<=ord(char)<=90 or 97<=ord(char)<=122): #verificar se so tem maiusculas, minusculas e espaços
         print("\nnome invalido: caracteres nao reconhecidos")
         exit()
         
 
 
-if '  ' in nome:
+if '  ' in nome: #verificar se tem mais do que um espaço
     print("\nNome invalido: espacos a mais entre os nomes")
     exit()
 
-if nome.count(' ')==0:
+if nome.count(' ')==0: #verificar se tem algum espaço
     print("\nnome invalido: falta espaco entre os nomes")
     exit()
 
-nomes_separados=nome.split()
+nomes_separados=nome.split() #isolar os nomes
 
-for nome_sep in nomes_separados:
+for nome_sep in nomes_separados: #verificar se em cada nome a primeira letra é maiscula e as restantes minusculas
 
     primeira_letra=nome_sep[0]
     
@@ -35,5 +35,5 @@ for nome_sep in nomes_separados:
             print("\nnome invalido: apenas a primeira letra pode ser maiscula")
             exit()
     
-print("\nNome valido")
+print("\nNome valido") #caso todos os criterios sejam verificados, dizer que o nome e valido e dar append a lista de nomes
 listanomes.append(nome)
